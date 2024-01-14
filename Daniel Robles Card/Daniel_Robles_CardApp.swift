b85d5d69959c49260ee63d56bb3d8b112fb3b22a
Camilo L-Shide <danielcamilorobles95@gmail.com>
2024-01-14 13:05:50 -0600

    Initial Commit

---
+++ Daniel Robles Card/Daniel_Robles_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daniel_Robles_CardApp.swift
+//  Daniel Robles Card
+//
+//  Created by Camilo L-Shide on 14/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Daniel_Robles_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
